1ce45c1d42d8bedabb239d84773ad850b21526f5
MorisakiYuji <youkourow@yahoo.co.jp>
2019-03-03 12:01:34 +0900

    ビルドした

--- dist/js/main.js
+++ dist/js/main.js
@@ -11615,20 +11615,152 @@ Object(_smoothScroll__WEBPACK_IMPORTED_MODULE_2__["smoothScroll"])();
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "smoothScroll", function() { return smoothScroll; });
+/*
+ *
+ * TERMS OF USE - EASING EQUATIONS
+ * 
+ * Open source under the BSD License. 
+ * 
+ * Copyright © 2001 Robert Penner
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of 
+ * conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list 
+ * of conditions and the following disclaimer in the documentation and/or other materials 
+ * provided with the distribution.
+ * 
+ * Neither the name of the author nor the names of contributors may be used to endorse 
+ * or promote products derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+ * OF THE POSSIBILITY OF SUCH DAMAGE. 
+ *
+ */
 function smoothScroll() {
   'use strict';
 
-  var Anc = document.getElementsByTagName('a');
-  console.log(Anc);
+  var anc = document.getElementsByTagName('a');
+  var ancLength = anc.length;
+  var i = 0;
+  var pageInnerAncList = []; // ページ内リンクの要素を取得
+
+  for (; i < ancLength; i++) {
+    var ancHref = anc[i].getAttribute('href');
+
+    if (ancHref.match(/^#/)) {
+      pageInnerAncList.push(anc[i]);
+    }
+  } // 機能実行
+
+
+  pageInnerAncList.forEach(function (i) {
+    setSmoothScroll(i);
+  });
 
   function setSmoothScroll(root, options) {
-    if (!root.length) {
+    if (!root) {
       return;
     }
 
     var o = {
-      root: root
+      duration: 500
     };
+
+    if (options) {
+      for (var key in options) {
+        o[key] = options[key];
+      }
+    }
+    /**
+     * スムーススクロール機能
+     * @constructor
+     */
+
+
+    var SmoothScroll = function SmoothScroll() {
+      this.root = root;
+      this.duration = o.duration;
+    };
+
+    SmoothScroll.prototype = {
+      init: function init() {
+        this.clickHandler();
+      },
+
+      /**
+       * イージング関数（easeInQuad）
+       * やっていないけど、オプションで拡張可能
+       * @param {number} t 経過時間
+       * @param {number} b スクロール開始位置
+       * @param {number} c スクロール到着位置
+       * @param {number} d デュレーション
+       * @return {number} 現在のイージング値
+       */
+      easing: function easing(t, b, c, d) {
+        return c * (t /= d) * t + b;
+      },
+      clickHandler: function clickHandler() {
+        var self = this;
+        this.root.addEventListener('click', function (e) {
+          e.preventDefault();
+          var thisEl = e.target;
+          var targetHash = thisEl.getAttribute('href');
+          var targetId = targetHash.replace(/^#/, '');
+          var target = document.getElementById(targetId);
+          var targetPosition = null;
+          var startPosition = window.pageYOffset || window.scrollY;
+          var elapsedTime = 0;
+          var next = null;
+          var timeStart = null;
+          var toTop = false;
+
+          var move = function move(timeCurrent) {
+            if (!timeStart) {
+              timeStart = timeCurrent;
+            }
+
+            if (elapsedTime >= self.duration) {
+              return;
+            }
+
+            if (toTop) {
+              history.pushState(null, null, '/');
+            } else {
+              history.pushState(null, null, targetHash);
+            }
+
+            elapsedTime = timeCurrent - timeStart;
+            next = self.easing(elapsedTime, startPosition, targetPosition, self.duration);
+            window.scrollTo(0, next);
+            requestAnimationFrame(move);
+          };
+
+          if (target) {
+            targetPosition = target.getBoundingClientRect().top;
+            move();
+          } else if (targetHash === '#') {
+            targetPosition = "-".concat(startPosition);
+            toTop = true;
+            move();
+          } else {
+            return;
+          }
+        }, false);
+      }
+    };
+    var scroll = new SmoothScroll();
+    scroll.init();
   }
 }
 
